825ada6cf37dab350ace3ef737944f59ccf16dd4
aizawa <ikiatawazia@outlook.jp>
2023-12-16 09:53:40 +0900

    管理画面で注文ステータスの変更ができるようにする

--- src/features/admin/components/OrderList.tsx
+++ src/features/admin/components/OrderList.tsx
@@ -1,7 +1,49 @@
 import { Button } from '@/components/ui/button';
+import { db } from '@/lib/firebase/sdk';
+import queryClient from '@/lib/react-query';
 import { orderHistoryProps } from '@/utils/enum';
+import { doc, updateDoc } from 'firebase/firestore';
 
-export const OrderList = ({ orderData }: { orderData: orderHistoryProps }) => {
+export const OrderList = ({
+  orderData,
+  refetch,
+}: {
+  orderData: orderHistoryProps;
+  refetch: any;
+}) => {
+  const OrderStatusButton = ({ orderStatus }: { orderStatus: number }) => {
+    if (orderStatus == 1) {
+      return (
+        <Button
+          className="bg-themeblue"
+          onClick={async () => {
+            await updateDoc(doc(db, 'orders', orderData.orderId), {
+              orderStatus: 2,
+            });
+            refetch();
+          }}
+        >
+          配達処理を開始
+        </Button>
+      );
+    } else if (orderStatus == 2 || orderStatus == 3) {
+      return (
+        <Button
+          className="bg-themeblue"
+          disabled={orderStatus == 3 ? true : false}
+          onClick={async () => {
+            await updateDoc(doc(db, 'orders', orderData.orderId), {
+              orderStatus: 3,
+            });
+            refetch();
+          }}
+        >
+          配達処理完了
+        </Button>
+      );
+    }
+  };
+
   return (
     <div>
       <div className="rounded border p-6">
@@ -34,7 +76,7 @@ export const OrderList = ({ orderData }: { orderData: orderHistoryProps }) => {
           <span className="font-semibold">コメント:</span> {orderData.comment}
         </p>
         <div className="space-x-4">
-          <Button className="bg-themeblue">注文ステータス</Button>
+          <OrderStatusButton orderStatus={orderData.orderStatus} />
           <Button variant="outline" className="border-themeblue">
             <p className="text-themeblue">コメントの変更</p>
           </Button>
--- src/features/admin/pages/Admin.tsx
+++ src/features/admin/pages/Admin.tsx
@@ -4,14 +4,14 @@ import { OrderList } from '../components/OrderList';
 import { ordersQueries } from '@/features/mypage/queries/orders';
 
 export const Admin = () => {
-  const { data } = useQuery({ ...ordersQueries.getAllOrders() });
+  const { data, refetch } = useQuery({ ...ordersQueries.getAllOrders() });
   const orderData = data?.results;
 
   return (
     <div className="mx-auto max-w-screen-2xl p-4">
       <div className="grid lg:grid-cols-2 gap-4">
         {orderData?.map((order, index) => (
-          <OrderList orderData={order} key={index} />
+          <OrderList orderData={order} refetch={refetch} key={index} />
         ))}
       </div>
     </div>
