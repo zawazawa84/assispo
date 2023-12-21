7102daf367278d452b27bd8c469e9c18ed8802ec
aizawa <ikiatawazia@outlook.jp>
2023-12-22 05:36:56 +0900

    orderSatusが1の時のみ振り込み期限を過ぎているかどうか確認

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,27 +7,30 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
-import * as admin from 'firebase-admin';
-import { format, subDays } from 'date-fns';
+import * as admin from "firebase-admin";
+import {format, subDays} from "date-fns";
 admin.initializeApp();
 
 const db = admin.firestore();
 
 exports.scheduledFunction = functions.pubsub
-  .schedule('every 1 minutes')
-  .timeZone('Asia/Tokyo')
+  .schedule("every 1 minutes")
+  .timeZone("Asia/Tokyo")
   .onRun(async () => {
     const nowUtc = new Date();
     const nowJst = new Date(nowUtc.getTime() + 9 * 60 * 60 * 1000);
 
     const fourDaysAgo = subDays(nowJst, 4);
-    const formattedDate = format(fourDaysAgo, 'yyyy.MM.dd');
+    const formattedDate = format(fourDaysAgo, "yyyy.MM.dd");
 
-    const snapshot = await db.collection('orders').get();
+    const snapshot = await db
+      .collection("orders")
+      .where("orderStatus", "==", 1)
+      .get();
     const deletions: any[] = [];
     snapshot.forEach((doc) => {
       const data = doc.data();
