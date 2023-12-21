a8f84d2ca86c4b17953187a3b1eb293e8e60825c
aizawa <ikiatawazia@outlook.jp>
2023-12-21 09:12:02 +0900

    とりあえず4日前の日付とdata.dateが一致したら削除（orderStatusが1の場合のみを条件として追加する）

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,36 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import * as functions from 'firebase-functions';
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
+import * as admin from 'firebase-admin';
+import { format, subDays } from 'date-fns';
+admin.initializeApp();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const db = admin.firestore();
+
+exports.scheduledFunction = functions.pubsub
+  .schedule('every 1 minutes')
+  .timeZone('Asia/Tokyo')
+  .onRun(async () => {
+    const nowUtc = new Date();
+    const nowJst = new Date(nowUtc.getTime() + 9 * 60 * 60 * 1000);
+
+    const fourDaysAgo = subDays(nowJst, 4);
+    const formattedDate = format(fourDaysAgo, 'yyyy.MM.dd');
+
+    const snapshot = await db.collection('orders').get();
+    const deletions: any[] = [];
+    snapshot.forEach((doc) => {
+      const data = doc.data();
+      console.log(data.date, formattedDate, nowJst);
+      if (data.date == formattedDate) {
+        deletions.push(doc.ref.delete());
+      }
+    });
+
+    await Promise.all(deletions);
+    return null;
+  });
