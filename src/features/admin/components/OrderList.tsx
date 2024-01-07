73881f2ddfcdf3df2fc4dbae2d1c29287eae0a57
aizawa <ikiatawazia@outlook.jp>
2024-01-08 02:24:54 +0900

    管理画面からコメントを編集できるようにする

--- src/features/admin/components/OrderList.tsx
+++ src/features/admin/components/OrderList.tsx
@@ -1,4 +1,14 @@
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { toast } from '@/components/ui/use-toast';
 import { db } from '@/lib/firebase/sdk';
 import queryClient from '@/lib/react-query';
@@ -7,7 +17,9 @@ import {
   orderHistoryProps,
   orderStatusProps,
 } from '@/utils/enum';
-import { doc, updateDoc } from 'firebase/firestore';
+import { doc, setDoc, updateDoc } from 'firebase/firestore';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 export const OrderList = ({
   orderData,
@@ -16,38 +28,22 @@ export const OrderList = ({
   orderData: orderHistoryProps;
   refetch: any;
 }) => {
-  const OrderStatusButton = ({ orderStatus }: { orderStatus: number }) => {
-    if (orderStatus == 1) {
-      return (
-        <Button
-          className="bg-themeblue"
-          onClick={async () => {
-            await updateDoc(doc(db, 'orders', orderData.orderId), {
-              orderStatus: 2,
-            });
-            refetch();
-          }}
-        >
-          配達処理を開始
-        </Button>
-      );
-    } else if (orderStatus == 2 || orderStatus == 3) {
-      return (
-        <Button
-          className="bg-themeblue"
-          disabled={orderStatus == 3 ? true : false}
-          onClick={async () => {
-            await updateDoc(doc(db, 'orders', orderData.orderId), {
-              orderStatus: 3,
-            });
-            refetch();
-          }}
-        >
-          配達処理完了
-        </Button>
-      );
-    }
-  };
+  const [editable, setEditable] = useState(false);
+
+  const {
+    formState: { errors, isSubmitting },
+    handleSubmit,
+    register,
+  } = useForm<{ comment: string }>();
+
+  const updateComment = handleSubmit(async (data) => {
+    await updateDoc(doc(db, 'orders', `${orderData.orderId}`), {
+      comment: data.comment,
+    });
+    setEditable(false);
+    refetch();
+    toast({ title: 'コメントを更新しました' });
+  });
 
   const cancelOrder = async () => {
     const orderDocRef = doc(db, 'orders', orderData.orderId);
@@ -60,45 +56,88 @@ export const OrderList = ({
   };
 
   return (
-    <div>
-      <div className="rounded border p-6">
-        <p className="text-gray-700 text-base mb-4">
+    <form onSubmit={updateComment}>
+      <div className="rounded border p-6 space-y-4">
+        <p className="text-gray-700 text-base">
           <span className="font-semibold">注文日</span>{' '}
           {orderData.date as unknown as string}
         </p>
-        <p className="text-gray-700 text-base mb-4">
+        <p className="text-gray-700 text-base">
           <span className="font-semibold">ユーザーID</span> {orderData.userId}
         </p>
-        <p className="text-gray-700 text-base mb-4">
+        <p className="text-gray-700 text-base">
           <span className="font-semibold">商品コード:</span>{' '}
           {orderData.productcode}
         </p>
-        <p className="text-gray-700 text-base mb-4">
+        <p className="text-gray-700 text-base">
           <span className="font-semibold">レンタル期間:</span> {orderData.term}
         </p>
-        <p className="text-gray-700 text-base mb-4">
+        <p className="flex items-center space-x-2 text-gray-700 text-base">
           <span className="font-semibold">注文ステータス:</span>{' '}
-          {numberToOrderStatus(
-            orderData.isCanceled
-              ? orderStatusProps.isCanceled
-              : orderData.orderStatus,
-          )}
+          <Select
+            defaultValue={String(
+              orderData.isCanceled
+                ? orderStatusProps.isCanceled
+                : orderData.orderStatus,
+            )}
+          >
+            <SelectTrigger className="w-48">
+              <SelectValue placeholder="サイズを選ぶ" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="0">キャンセル済</SelectItem>
+                <SelectItem value="1">代金未払い</SelectItem>
+                <SelectItem value="2">配達処理中</SelectItem>
+                <SelectItem value="3">配達中</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </p>
-        <p className="text-gray-700 text-base mb-4">
+        <p className="flex items-center space-x-2 text-gray-700 text-base ">
           <span className="font-semibold">返却ステータス:</span>{' '}
-          {orderData.returnStatus}
+          <Select defaultValue={String(orderData.returnStatus)}>
+            <SelectTrigger className="w-48">
+              <SelectValue placeholder="サイズを選ぶ" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="1">未選択</SelectItem>
+                <SelectItem value="2">レンタル中</SelectItem>
+                <SelectItem value="3">返却手続き済</SelectItem>
+                <SelectItem value="4">返却済</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </p>
-        <p className="text-gray-700 text-base mb-4">
+        <p className="text-gray-700 text-base ">
           <span className="font-semibold">配送先:</span> {orderData.toAddress}
         </p>
-        <p className="text-gray-700 text-base mb-4">
-          <span className="font-semibold">コメント:</span> {orderData.comment}
+        <p className="flex items-center space-x-2 text-gray-700 text-base whitespace-nowrap">
+          <span className="font-semibold">コメント:</span>{' '}
+          {!editable ? (
+            orderData.comment
+          ) : (
+            <Input className="w-full" {...register('comment')} />
+          )}
         </p>
         <div className="space-x-4">
-          <OrderStatusButton orderStatus={orderData.orderStatus} />
-          <Button variant="outline" className="border-themeblue">
-            <p className="text-themeblue">コメントの変更</p>
-          </Button>
+          {!editable ? (
+            <Button
+              variant="outline"
+              className="border-themeblue"
+              onClick={(e) => {
+                e.preventDefault();
+                setEditable((prev) => !prev);
+              }}
+            >
+              <p className="text-themeblue">コメントの変更</p>
+            </Button>
+          ) : (
+            <Button className="bg-themeblue" type="submit">
+              コメントの保存
+            </Button>
+          )}
           <Button
             variant="outline"
             className="border-destructive"
@@ -109,6 +148,6 @@ export const OrderList = ({
           </Button>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
