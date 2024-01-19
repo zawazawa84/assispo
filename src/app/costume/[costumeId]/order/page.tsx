fec8a8e357aa80a19deea0e8d089a0d0316892e1
aizawa <ikiatawazia@outlook.jp>
2024-01-20 04:00:33 +0900

    手続きの際住所が登録されていないとマイページへリダイレクト

--- src/app/costume/[costumeId]/order/page.tsx
+++ src/app/costume/[costumeId]/order/page.tsx
@@ -1,5 +1,13 @@
+'use client';
+import { useAuthContext } from '@/AuthContext';
+import { toast } from '@/components/ui/use-toast';
 import { CostumeOrder } from '@/features/costume/pages/CostumeOrder';
+import { redirect } from 'next/navigation';
 
 export default function CostumeOrderPage() {
+  const { userData } = useAuthContext()!;
+  if (!userData?.address) {
+    redirect('/mypage');
+  }
   return <CostumeOrder />;
 }
