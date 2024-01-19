6ed1115587bfe4f1b3dfb79360ce7229b0c4d2d1
aizawa <ikiatawazia@outlook.jp>
2024-01-20 04:22:53 +0900

    あるアカウントじゃないと管理画面に到達できないようにする

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,17 @@
+'use client';
+import { useAuthContext } from '@/AuthContext';
 import { Admin } from '@/features/admin/pages/Admin';
+import { redirect } from 'next/navigation';
 
 export default function AdminPage() {
+  const { user } = useAuthContext()!;
+  if (!user) {
+    return <div>403 Forbidden</div>;
+  }
+
+  if (user?.uid != 'ihNdA7V5umNojt4QMzrz6Zr2nUl1') {
+    redirect('/costume');
+  }
+
   return <Admin />;
 }
