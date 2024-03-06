0e2949d3ee6238f55fdad6813fe78da5fa5b8642
aizawa <aizawa.job84@gmail.com>
2024-03-06 18:59:09 +0900

    本番環境と開発環境を分ける

--- src/lib/firebase/sdk.ts
+++ src/lib/firebase/sdk.ts
@@ -3,15 +3,28 @@ import { getFirestore } from 'firebase/firestore';
 // import { getAnalytics } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAcbMlwWvUORBf_HSurF8weW9hP-nHzzNA',
-  authDomain: 'assispo-a2907.firebaseapp.com',
-  projectId: 'assispo-a2907',
-  storageBucket: 'assispo-a2907.appspot.com',
-  messagingSenderId: '361701071989',
-  appId: '1:361701071989:web:0100a845508938c1b96c3c',
-  measurementId: 'G-C500MNKGPD',
-};
+let firebaseConfig;
+if (process.env.NODE_ENV === 'production') {
+  firebaseConfig = {
+    apiKey: 'AIzaSyDlpO7LD5tjic8GIY8DH3CeWoucRzGgYPA',
+    authDomain: 'asispo-78656.firebaseapp.com',
+    projectId: 'asispo-78656',
+    storageBucket: 'asispo-78656.appspot.com',
+    messagingSenderId: '491602287606',
+    appId: '1:491602287606:web:3ae69c3da6f533405645d7',
+    measurementId: 'G-2156GGVN0T',
+  };
+} else {
+  firebaseConfig = {
+    apiKey: 'AIzaSyAcbMlwWvUORBf_HSurF8weW9hP-nHzzNA',
+    authDomain: 'assispo-a2907.firebaseapp.com',
+    projectId: 'assispo-a2907',
+    storageBucket: 'assispo-a2907.appspot.com',
+    messagingSenderId: '361701071989',
+    appId: '1:361701071989:web:0100a845508938c1b96c3c',
+    measurementId: 'G-C500MNKGPD',
+  };
+}
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
